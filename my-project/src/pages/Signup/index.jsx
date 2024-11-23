09fe25ebe71f3b49a8f47b4bf43ed758325956f1
Manas-Kumar-Lal <manaslal2004@gmail.com>
2024-11-23 19:05:28 +0530

    first

--- my-project/src/pages/Signup/index.jsx
+++ my-project/src/pages/Signup/index.jsx
@@ -1,32 +1,37 @@
 import { Link } from "react-router-dom";
 import styles from "./styles.module.css";
 
-function Login() {
+function Signup() {
 	const googleAuth = () => {
 		window.open(
-			`${process.env.REACT_APP_API_URL}/auth/google/callback`,
+			`${import.meta.env.VITE_DB_URI}/auth/google/callback`,
 			"_self"
 		);
 	};
 	return (
 		<div className={styles.container}>
-			<h1 className={styles.heading}>Log in Form</h1>
+			<h1 className={styles.heading}>Sign up Form</h1>
 			<div className={styles.form_container}>
 				<div className={styles.left}>
-					<img className={styles.img} src="./images/login.jpg" alt="login" />
+					<img className={styles.img} src="./images/signup.jpg" alt="signup" />
 				</div>
 				<div className={styles.right}>
-					<h2 className={styles.from_heading}>Members Log in</h2>
+					<h2 className={styles.from_heading}>Create Account</h2>
+					<input type="text" className={styles.input} placeholder="Username" />
 					<input type="text" className={styles.input} placeholder="Email" />
-					<input type="text" className={styles.input} placeholder="Password" />
-					<button className={styles.btn}>Log In</button>
+					<input
+						type="password"
+						className={styles.input}
+						placeholder="Password"
+					/>
+					<button className={styles.btn}>Sign Up</button>
 					<p className={styles.text}>or</p>
 					<button className={styles.google_btn} onClick={googleAuth}>
 						<img src="./images/google.png" alt="google icon" />
-						<span>Sing in with Google</span>
+						<span>Sing up with Google</span>
 					</button>
 					<p className={styles.text}>
-						New Here ? <Link to="/signup">Sing Up</Link>
+						Already Have Account ? <Link to="/login">Log In</Link>
 					</p>
 				</div>
 			</div>
@@ -34,4 +39,4 @@ function Login() {
 	);
 }
 
-export default Login;
+export default Signup;
